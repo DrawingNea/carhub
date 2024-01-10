43436b7fb27656c24f618d2dd22e1009dca288c6
DrawingNea <lcy.li@outlook.de>
2024-01-10 13:03:07 +0100

    Change Custom Button and create file for type interfaces

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+"use client"
 
-const CustomButton = () => {
+import { CustomButtonProps } from "@/types";
+import Image from "next/image";
+
+const CustomButton = ({ title, containerStyles, handleClick }: CustomButtonProps) => {
   return (
-    <div>CustomButton</div>
-  )
-}
+    <button
+      disabled={false}
+      type={"button"}
+      className={`custom-btn`}
+      onClick={() => {}}
+    >
+      <span className={`flex-1`}>
+        Title
+      </span>
+    </button>
+  );
+};
 
-export default CustomButton
+export default CustomButton;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,11 @@
+"use client"
+
 import React from "react";
 import Image from "next/image";
 import  CustomButton  from "./CustomButton";
 
 const Hero = () => {
+  const handleScroll = () => {}
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
@@ -13,7 +16,7 @@ const Hero = () => {
           Streamline your car rental experience with our effortless booking
           process.
         </p>
-        <CustomButton />
+        <CustomButton title="Explore Cars" containerStyles="bg-primary-blue text-white rounded-full mt-10" handleClick={handleScroll} />
       </div>
     </div>
   );
