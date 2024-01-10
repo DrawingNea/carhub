178a3bcac1544f56cd54c68e755200f4ec115ff8
DrawingNea <lcy.li@outlook.de>
2024-01-10 13:09:18 +0100

    Add images to hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 
 import React from "react";
 import Image from "next/image";
-import  CustomButton  from "./CustomButton";
+import CustomButton from "./CustomButton";
 
 const Hero = () => {
-  const handleScroll = () => {}
+  const handleScroll = () => {};
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
@@ -16,7 +16,17 @@ const Hero = () => {
           Streamline your car rental experience with our effortless booking
           process.
         </p>
-        <CustomButton title="Explore Cars" containerStyles="bg-primary-blue text-white rounded-full mt-10" handleClick={handleScroll} />
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay" />
+        </div>
       </div>
     </div>
   );
