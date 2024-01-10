18c242547024c51570e0ce722dd74e72517f1184
DrawingNea <lcy.li@outlook.de>
2024-01-10 12:54:51 +0100

    Add base Custom Botton component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import  Image  from "next/image"
+import React from "react";
+import Image from "next/image";
+import  CustomButton  from "./CustomButton";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book or rent a car — quickly and easily!
+        </h1>
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our effortless booking
+          process.
+        </p>
+        <CustomButton />
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
