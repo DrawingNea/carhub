8d76bd164d9879b2b270d192c7d2295e1e6c0cfb
DrawingNea <lcy.li@outlook.de>
2024-01-11 15:37:43 +0100

    Fix dialog panel styles

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -38,7 +38,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="relative w-full max-w-lg max-h-[90h] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xsl transition-all flex flex-col gap-5">
+                <Dialog.Panel className="relative w-full max-w-lg max-h-[90h] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5">
                   <button
                     type="button"
                     onClick={closeModal}
