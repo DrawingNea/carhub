a34692d104c5e8852788034d9b662d005545574b
DrawingNea <lcy.li@outlook.de>
2024-01-11 11:50:25 +0100

    Add content to car card from props

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+"use client";
 
-const CarCard = () => {
-  return (
-    <div>CarCard</div>
-  )
+import { useState } from "react";
+import Image from "next/image";
+import { CarProps } from "@/types";
+import { CustomButton } from "./";
+import { calculateCarRent } from "@/utils";
+
+interface CarCardProps {
+  car: CarProps;
 }
 
-export default CarCard
+const CarCard = ({ car }: CarCardProps) => {
+  const { city_mpg, year, make, model, transmission, drive } = car;
+  const carRent = calculateCarRent(city_mpg, year);
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
+      <p className="flex mt-6 text-[32px] font-extrabold">
+        <span className="self-start text-[14px] font-semibold">$</span>
+        {carRent}
+        <span className="self-end text-[14px] font-medium">/day</span>
+      </p>
+      <div className="relative w-full h-40 my-3 object-contain">
+        <Image
+          src="/hero.png"
+          alt="Car model"
+          fill
+          priority
+          className="object-contain"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CarCard;
