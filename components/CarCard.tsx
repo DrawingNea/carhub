3a4e512635c58a5983a34aa3038f36ffc0cfd0b0
DrawingNea <lcy.li@outlook.de>
2024-01-11 15:28:17 +0100

    Make backdrop for modal and top of modal car image

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -63,7 +63,7 @@ const CarCard = ({ car }: CarCardProps) => {
             <CustomButton title="View More" containerStyles="w-full py-[16px] rounded-full bg-primary-blue" textStyles="text-white text-[14px] leading-[17px] font-bold" rightIcon="/right-arrow.svg" handleClick={() => setIsOpen(true)}/>
         </div>
       </div>
-      <CarDetails />
+      <CarDetails isOpen={isOpen} closeModal={()=>setIsOpen(false)} car={car} />
     </div>
   );
 };
