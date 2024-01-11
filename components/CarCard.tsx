3c56c961c6eb785d69fbd7790800cb32f36470b8
DrawingNea <lcy.li@outlook.de>
2024-01-11 15:58:42 +0100

    Add generated car images to Car Card

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import Image from "next/image";
 import { CarProps } from "@/types";
 import { CarDetails, CustomButton } from "./";
-import { calculateCarRent } from "@/utils";
+import { calculateCarRent, generateCarImageUrl } from "@/utils";
 
 interface CarCardProps {
   car: CarProps;
@@ -28,7 +28,7 @@ const CarCard = ({ car }: CarCardProps) => {
       </p>
       <div className="relative w-full h-40 my-3 object-contain">
         <Image
-          src="/hero.png"
+          src={generateCarImageUrl(car)}
           alt="Car model"
           fill
           priority
