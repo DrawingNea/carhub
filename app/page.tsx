9edad95d2533ad285bc0409644683e9689fd8d0d
DrawingNea <lcy.li@outlook.de>
2024-01-12 13:38:03 +0100

    Add base show more component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from "@/components";
 import { fuels, yearsOfProduction } from "@/constants";
 import { fetchCars } from "@/utils";
 import Image from "next/image";
@@ -34,6 +34,7 @@ export default async function Home({ searchParams }) {
                 <CarCard car={car} />
               ))}
             </div>
+            <ShowMore />
           </section>
         ) : (
           <div className="home__error-container">
