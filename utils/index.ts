6885665ce0a170737cbee830c95012e2219ef945
DrawingNea <lcy.li@outlook.de>
2024-01-12 13:34:08 +0100

    Create util function update Search Params

--- utils/index.ts
+++ utils/index.ts
@@ -32,29 +32,22 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   return rentalRatePerDay.toFixed(0);
 };
 
-export const updateSearchParams = (type: string, value: string) => {
-  // Get the current URL search params
-  const searchParams = new URLSearchParams(window.location.search);
-
-  // Set the specified search parameter to the given value
-  searchParams.set(type, value);
-
-  // Set the specified search parameter to the given value
-  const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
-
-  return newPathname;
-};
-
-
 export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   const url = new URL("https://cdn.imagin.studio/getImage");
-  const { make, year , model } = car;
+  const { make, year, model } = car;
 
-  url.searchParams.append("customer",process.env.NEXT_PUBLIC_IMAGIN_API_KEY!);
-  url.searchParams.append('make', make);
-  url.searchParams.append('modelFamily', model.split(" ")[0]);
-  url.searchParams.append('zoomType', 'fullscreen');
-  url.searchParams.append('modelYear', `${year}`);
-  url.searchParams.append('angle', `${angle}`);
+  url.searchParams.append("customer", process.env.NEXT_PUBLIC_IMAGIN_API_KEY!);
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle}`);
   return `${url}`;
-}
+};
+
+export const updateSearchParams = (type: string, value: string) => {
+  const searchParams = new URLSearchParams(window.location.search);
+  searchParams.set(type, value);
+  const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+  return newPathname;
+};
