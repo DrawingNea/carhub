8be539354b383323716a46edc6f8e644b7e080d2
DrawingNea <lcy.li@outlook.de>
2024-01-12 13:35:18 +0100

    Create filter logic for custom filter by fuel and  year

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { fuels, yearsOfProduction } from "@/constants";
 import { fetchCars } from "@/utils";
 import Image from "next/image";
 
@@ -22,8 +23,8 @@ export default async function Home({ searchParams }) {
         <div className="home__filters">
           <SearchBar />
           <div className="home__filter-container">
-            <CustomFilter title="fuel" />
-            <CustomFilter title="year" />
+            <CustomFilter title="fuel" options ={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
         {!isDataEmpty ? (
--- types/index.ts
+++ types/index.ts
@@ -37,3 +37,13 @@ export interface FilterProps {
   fuel: string;
   limit: number;
 }
+
+export interface OptionProps {
+  title: string;
+  value: string;
+}
+
+export interface CustomFilterProps{
+  title: string;
+  options: OptionProps[];
+}
